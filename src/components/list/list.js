3cebf9d738a00417cda76e5f7b47635383d46561
宁宁 <383514580@qq.com>
2017-02-22 11:59:40 +0800

    no message

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,13 +1,21 @@
 import lodash from 'lodash'
 // var lodash = require('lodash');
 module.exports = function() {
-    // 写list组件的js代码
-    // console.log(lodash);
 
-    function add(x, y) {
-        alert(x + y);
+    "use strict";
+
+    function _toConsumableArray(arr) {
+        if (Array.isArray(arr)) {
+            for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+            	arr2[i] = arr[i]; 
+         	}
+            return arr2;
+        } else {
+            return Array.from(arr);
+        }
     }
-    
-    add(...[1,2])
+
+    var set = new Set([1, 2, 3, 4, 4]);
+    [].concat(_toConsumableArray(set));
 
 }
